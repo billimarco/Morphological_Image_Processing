6972cb7b775a394178652571501b082a758a1879
Marco <marcolbilli@gmail.com>
2025-05-12 19:04:16 +0200

    Aggiunto anche il plot per i tempi

--- mainPaper2.py
+++ mainPaper2.py
@@ -6,7 +6,8 @@ import os
 dimensioni = ['400x400', '1600x1600', '6400x6400']
 dischi = ['disk1', 'disk5']
 versioni = ['V1', 'V2', 'V3']
-metrica = 'Total'  # Cambia in 'Total' per plottare Erosion/Dilation Total
+
+metrica = '_Total'  # Cambia in '_Total' per plottare Erosion/Dilation Total altrimenti ''
 
 # Colori univoci per ogni combinazione versione-operazione
 tab10 = plt.get_cmap('tab10').colors
@@ -15,7 +16,7 @@ linee = [(v, op) for v in versioni for op in operazioni]
 colori_linee = {f'{op}_{v}': tab10[i % len(tab10)] for i, (v, op) in enumerate(linee)}
 
 # Crea la figura: righe = dischi, colonne = dimensioni
-fig, axes = plt.subplots(len(dischi), len(dimensioni), figsize=(11.69, 8.27), sharex=True, sharey=True)
+fig, axes = plt.subplots(len(dischi), len(dimensioni), figsize=(11.69, 8.27), sharex=True, sharey=False)
 
 # Cicla su tutte le combinazioni
 for row, disco in enumerate(dischi):
@@ -23,20 +24,19 @@ for row, disco in enumerate(dischi):
         ax = axes[row, col] if len(dischi) > 1 else axes[col]
 
         for v in versioni:
-            file_path = f'build/Release/results/{dimensione}_{disco}/csv_speedup_{v}_{dimensione}_{disco}.csv'
+            file_path = f'build/Release/results/{dimensione}_{disco}/csv_times_{v}_{dimensione}_{disco}.csv'
             df = pd.read_csv(file_path)
-
             for op in operazioni:
-                col_key = f'{op[0]}_{metrica}'  # E_Mean o E_Total, ecc.
+                col_key = f'{op[0]}{metrica}_Par'  # E_Mean o E_Total, ecc.
                 label = f'{op} ({v})'
                 color = colori_linee[f'{op}_{v}']
                 ax.plot(df['Threads'], df[col_key], label=label, color=color, linestyle='-', marker='o')
-
+                
         # Titoli e assi
         if row == 0:
             ax.set_title(f'{dimensione}')
         if col == 0:
-            ax.set_ylabel(f'{disco}\nSpeedup')
+            ax.set_ylabel(f'{disco}\nTime (s)')
         if row == len(dischi) - 1:
             ax.set_xlabel('Threads')
 
@@ -48,7 +48,10 @@ handles, labels = axes[0, 0].get_legend_handles_labels() if len(dischi) > 1 else
 fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, 0.96), ncol=3, fontsize='medium')
 
 # Titolo e layout
-plt.suptitle(f'Confronto Erosion/Dilation {metrica} - Versioni V1, V2, V3', fontsize=14)
+if metrica == '_Total':
+    plt.suptitle(f'Confronto Erosion/Dilation Total - Versioni V1, V2, V3', fontsize=14)
+else:
+    plt.suptitle(f'Confronto Erosion/Dilation Mean - Versioni V1, V2, V3', fontsize=14)
 plt.tight_layout(rect=[0, 0, 1, 0.93])
 plt.show()
 
